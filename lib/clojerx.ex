796dcad38cf4ca9d321e5ce1243e67803ac1ab6f
Jovany Leandro G.C <bit4bit@riseup.net>
2025-12-20 17:01:59 +0200

    first commit

---
+++ lib/clojerx.ex
@@ -0,0 +1,18 @@
+defmodule Clojerx do
+  @moduledoc """
+  Documentation for `Clojerx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clojerx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clojerx_test.exs
@@ -0,0 +1,8 @@
+defmodule ClojerxTest do
+  use ExUnit.Case
+  doctest Clojerx
+
+  test "greets the world" do
+    assert Clojerx.hello() == :world
+  end
+end
